96a6b69b24aadae4bc42436e4b15301bcc3949e0
Matheus Melo <mtsmelo95@gmail.com>
2023-09-10 09:54:37 -0300

    adicionando os arquivos do projeto

---
+++ src/CostSystem.java
@@ -0,0 +1,67 @@
+import java.util.LinkedList;
+
+public class CostSystem {
+  public static Object out;
+  LinkedList<Employee> employee = new LinkedList<>();
+  LinkedList<Department> department = new LinkedList<>();
+
+  /**
+   * 
+   */
+  public void newCostRecord() {} //Nicolli
+  
+  /**
+   * 
+   */
+  public void chooseEmployee() {}//Matheus
+  
+  /**
+   * 
+   */
+  public void findCostRecordByDate() {}//Augusto
+  
+  /**
+   * 
+   */
+  public void findCostRecordByCategory() {}//Daniele
+  
+  /**
+   * 
+   */
+  public void findCostRecordByDepartment() {}//Lucas
+  
+  /**
+   * 
+   */
+  public void deleteRecord() {}//Oliver
+  
+  /**
+   * 
+   */
+  public void employeeCurrentlyLoggedIn() {}//Nicolli
+  
+  /**
+   * 
+   */
+  public void totalCostsForTheMonth() {}//Matheus
+  
+  /**
+   * 
+   */
+  public void totalCostsForTheLast3Months() {}//Augusto
+  
+  /**
+   * 
+   */
+  public void employeesWithHighestSumOfRecordedCosts() {}//Daniele
+  
+  /**
+   * 
+   */
+  public void chooseFunctionality1() {}//Lucas
+  
+  /**
+   * 
+   */
+  public void chooseFunctionality2() {}//Oliver
+}
---
+++ src/Menu.java
@@ -0,0 +1,5 @@
+public class Menu {
+  CostSystem system = new CostSystem();
+
+  //AQUI MONTAMOS O MENU
+}
