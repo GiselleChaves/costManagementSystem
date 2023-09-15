e58d8f429df30c5ed92631d0b1954965f7043d8b
Matheus Melo <mtsmelo95@gmail.com>
2023-09-15 16:17:01 -0300

    adicionando task chooseEmployee

--- src/CostSystem.java
+++ src/CostSystem.java
@@ -8,60 +8,76 @@ public class CostSystem {
   /**
    * 
    */
-  public void newCostRecord() {} //Nicolli
-  
+  public void newCostRecord() {
+  } // Nicolli
+
   /**
    * 
    */
-  public void chooseEmployee() {}//Matheus
-  
+  public void chooseEmployee() {
+    for (Employee employee : listemployee) {
+      if (employee.getRegister() == cod)
+        System.out.println(employee);
+    }
+  }
+
   /**
    * 
    */
-  public void findCostRecordByDate() {}//Augusto
-  
+  public void findCostRecordByDate() {
+  }// Augusto
+
   /**
    * 
    */
-  public void findCostRecordByCategory() {}//Daniele
-  
+  public void findCostRecordByCategory() {
+  }// Daniele
+
   /**
    * 
    */
-  public void findCostRecordByDepartment() {}//Lucas
-  
+  public void findCostRecordByDepartment() {
+  }// Lucas
+
   /**
    * 
    */
-  public void deleteRecord() {}//Oliver
-  
+  public void deleteRecord() {
+  }// Oliver
+
   /**
    * 
    */
-  public void employeeCurrentlyLoggedIn() {}//Nicolli
-  
+  public void employeeCurrentlyLoggedIn() {
+  }// Nicolli
+
   /**
    * 
    */
-  public void totalCostsForTheMonth() {}//Matheus
-  
+  public void totalCostsForTheMonth() {
+  }// Matheus
+
   /**
    * 
    */
-  public void totalCostsForTheLast3Months() {}//Augusto
-  
+  public void totalCostsForTheLast3Months() {
+  }// Augusto
+
   /**
    * 
    */
-  public void employeesWithHighestSumOfRecordedCosts() {}//Daniele
-  
+  public void employeesWithHighestSumOfRecordedCosts() {
+  }// Daniele
+
   /**
    * 
    */
-  public void chooseFunctionality1() {}//Lucas
-  
+  public void chooseFunctionality1() {
+  }// Lucas
+
   /**
    * 
    */
-  public void chooseFunctionality2() {}//Oliver
+  public void chooseFunctionality2() {
+  }// Oliver
 }
