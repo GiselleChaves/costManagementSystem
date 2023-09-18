c0569148c63dc9311a67b636a113c4290018251e
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2023-09-18 20:27:15 -0300

    Transforming Department in a Enum Class

--- src/Department.java
+++ src/Department.java
@@ -1,26 +1,3 @@
-public class Department {
-  private Department deparmentName;
-
-  /**
-   * @param deparmentName
-   */
-  public Department(Department deparmentName) {
-    if(this.deparmentName == null) {
-      this.deparmentName = new Department(deparmentName);
-    }
-  }
-
-  /**
-   * @return
-   */
-  public Department getDeparmentName() {
-    return deparmentName;
-  }
-
-  /**
-   * @param departmentName
-   */
-  public void setDeparmentName(Department deparmentName) {
-    this.deparmentName = deparmentName;
-  }
+public enum DepartmentEnum {
+  FINANCIAL, IT, ACCOUNTING, PURCHASING;
 }
